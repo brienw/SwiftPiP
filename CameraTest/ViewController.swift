52b020c8ebce2beced97544409ef18ab8ad56c39
Brien Wankel <brien@reebosak.net>
2015-03-20 19:48:19 -0700

    Initial Commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Brien Wankel on 3/20/15.
+//  Copyright (c) 2015 Gnomie Kids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
